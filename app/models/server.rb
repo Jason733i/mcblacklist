f8cc7eb0591da1c8c4b0ea75a42e99d7d873a3c5
Jason733i <uniqueberry@gmail.com>
2011-09-18 21:13:36 -0400

    Styling fixes to the server index table, validation error box, forms.  Also added one validation for presence of name and ip_address.

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,6 +2,7 @@ class Server < ActiveRecord::Base
   IP_ADDRESS_FORMAT = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})?$/
   belongs_to :user
 
+  validates_presence_of :name, :ip_address
+  validates_format_of :ip_address, :with => IP_ADDRESS_FORMAT, :message => "format is incorrect, try (192.168.0.1)"
   validates_numericality_of :port, :greater_than => 0
-  validates_format_of :ip_address, :with => IP_ADDRESS_FORMAT, :message => "Ip address must be in the format XXX.XXX.XXX.XXX"
 end
